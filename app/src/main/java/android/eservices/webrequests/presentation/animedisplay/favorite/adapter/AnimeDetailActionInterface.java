db7e9f71f67946dbf4ebfbf11015be7d1517c9e1
Silicca <floriane.carton.etu@univ-lille.fr>
2020-01-19 12:37:05 +0100

    delete all for apply gitignore - bad developper

--- app/src/main/java/android/eservices/webrequests/presentation/animedisplay/favorite/adapter/AnimeDetailActionInterface.java
+++
@@ -1,5 +0,0 @@
-package android.eservices.webrequests.presentation.animedisplay.favorite.adapter;
-
-public interface AnimeDetailActionInterface {
-    void onRemoveFavorite(String animeId);
-}
--- app/src/main/java/android/eservices/webrequests/presentation/animedisplay/search/adapter/AnimeActionInterface.java
+++
@@ -1,6 +0,0 @@
-package android.eservices.webrequests.presentation.animedisplay.search.adapter;
-
-public interface AnimeActionInterface {
-
-    void onFavoriteToggle(String animeId, boolean isFavorite);
-}
